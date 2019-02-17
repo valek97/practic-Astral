326736397333c15e7fd8b6242dc9ee22de31ebe9
kritenko_vg <kritenko97@gmail.com>
2019-02-17 23:54:02 +0300

    Добавил БД. Выдает ошибку

---
+++ Тест пример/BooksStore/BooksStore/Models/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BooksStore.Models
+{
+    public class Book
+    {
+
+
+        // id книг
+        public int id { get; set; }
+
+        // наименование книги
+        public string Name { get; set; }
+
+        // Автор\ы
+        public string Author { get; set; }
+
+        // Стоимость
+        public int Price { get; set; }
+    }
+}
---
+++ Тест пример/BooksStore/BooksStore/Models/Purchase.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BooksStore.Models
+{
+    public class Purchase
+    {
+
+        //Id покупки
+        public int PurchaseId { get; set; }
+
+        //ФИО покупателя
+        public string Person { get; set; }
+
+        //Адрес покупателя
+        public string Addres { get; set; }
+
+        //Id книги
+        public int BookId { get; set; }
+
+        //Дата покупки
+        public DateTime Time { get; set; }
+
+
+    }
+}
