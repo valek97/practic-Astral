5d0e5776363d46145d7f0b3b6a24d20869fff908
kritenko_vg <kritenko97@gmail.com>
2019-02-25 13:30:35 +0300

    получение объектов из бд

--- Тест 2/test2/test2/Controllers/HomeController.cs
+++ Тест 2/test2/test2/Controllers/HomeController.cs
@@ -3,28 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using test2.Models;
 
 namespace test2.Controllers
 {
     public class HomeController : Controller
     {
+
+        //Создание контекста данных
+        PriceContext db = new PriceContext();
+
+
         public ActionResult Index()
         {
-            return View();
-        }
+            //получение из бд все объекты Tarif
+            IEnumerable<Tarif> tarifs = db.Tarif;
+            //получение из бд все объекты Tarif_atribute
+            IEnumerable<Tarif_atribute> Tarif_atributes = db.Tarif_atribute;
+            //получение из бд все объекты Price_tarif
+            IEnumerable<Price_tarif> Price_tarifs = db.Price_tarif;
+            //получение из бд все объекты Tarif
+            IEnumerable<Price_trend> Price_trend = db.Price_trend;
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
 
-            return View();
-        }
-
-        public ActionResult Contact()
-        {
-            ViewBag.Message = "Your contact page.";
 
             return View();
         }
+
+        
     }
 }
