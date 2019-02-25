efeff7d4eb8c32d949af91cd528460b1e1c24132
kritenko_vg <kritenko97@gmail.com>
2019-02-25 12:11:20 +0300

    4 модели и контекст данных

---
+++ Тест 2/test2/test2/Models/PriceContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace test2.Models
+{
+    public class PriceContext : DbContext
+    {
+
+        public DbSet<Price_tarif> Price_tarif { get; set; }
+        public DbSet<Price_trend> Price_trend { get; set; }
+        public DbSet<Tarif> Tarif { get; set; }
+        public DbSet<Tarif_atribute> Tarif_atribute { get; set; }
+    }
+}
